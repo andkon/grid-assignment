ab63584b4eec1c01a918acd9019ea5380b72fafc
Andrew Konoff <andrew.konoff@gmail.com>
2018-04-29 21:50:13 -0700

    Moved model into index.js, added each and if to template

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,28 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [{
+      isList: true,
+      x: 1,
+      y: 0,
+      width: 1,
+      height: 1
+    }, {
+      isMap: true,
+      x: 2,
+      y: 0,
+      width: 2,
+      height: 2
+    }, {
+      isList: true,
+      x: 0,
+      y: 3,
+      width: 1,
+      height: 1
+    }
+    ]
+  },
   actions: {
     change(event, items) {
       // Here, any changes to the grid items will bubble up.
